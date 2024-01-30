2c943909ce3e6dfe920420579efb84d71831ffba
viktorhugo <viktor1103@gmail.com>
2024-01-30 00:00:53 -0500

    edit chat screen

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,13 +2,13 @@ import 'package:chat_app/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
+  initialLocation: '/chat',
   routes: [
-
     // home
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const UsersScreen()
-    ),
+    // GoRoute(
+    //   path: '/',
+    //   builder: (context, state) => const ChatScreen()
+    // ),
     GoRoute(
       path: '/users',
       builder: (context, state) => const UsersScreen()
--- lib/presentation/screens/chat_screen.dart
+++ lib/presentation/screens/chat_screen.dart
@@ -7,9 +7,46 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('ChatScreen'),
+
+    final colors = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: colors.primary,
+        title: Column(
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.blue[100],
+              maxRadius: 14,
+              child: const Text('Vm', style: TextStyle(fontSize: 12),),
+            ),
+            const SizedBox(height: 3,),
+            const Text('Victor Mosquera', style: TextStyle(color: Colors.white, fontSize: 14))
+          ],
+        ),
+        centerTitle: true,
+        elevation: 1,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+
+            Flexible(
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, index) => Text('$index'),
+                reverse: true,
+              )
+            ),
+
+            const Divider(height: 1,),
+
+            Container(
+              color: Colors.white,
+              height: 100,
+            )
+          ],
+        ),
     ),
   );
   }
